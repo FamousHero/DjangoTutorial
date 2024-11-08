96683833db424f196167363937f1618a0e640a79
Daniel Franco <danielfranco2011@gmail.com>
2024-11-07 17:06:54 -0800

    [network_connections]: Create CableForm

---
+++ network_connections/forms.py
@@ -0,0 +1,11 @@
+from django.forms import ModelForm
+from .models import Cable
+
+class CableForm(ModelForm):
+    class Meta:
+        model = Cable
+        fields = ["device_1", "port_1", 
+                  "device_2", "port_2",
+                  "cable_type", "cable_length"]
+
+    template_name = "network_connections/forms/cable_form_snippet.html" 
